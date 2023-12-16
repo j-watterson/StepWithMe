1fa0133b7e28bf1c5e6885f55750279593df4a87
j-watterson <jonathonwatterson@gmail.com>
2023-12-16 12:37:57 -0800

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,22 @@
+// redirect.js
+function getDynamicUrl() {
+    // List of URLs to choose from
+    var urls = [
+        "https://example1.com",
+        "https://example2.com",
+        "https://example3.com",
+        // Add more URLs as needed
+    ];
+
+    // Randomly select a URL from the list
+    var randomIndex = Math.floor(Math.random() * urls.length);
+    return urls[randomIndex];
+}
+
+function redirectToDynamicUrl() {
+    var url = getDynamicUrl();
+    window.location.href = url;
+}
+
+// When the window loads, execute the redirect
+window.onload = redirectToDynamicUrl;
