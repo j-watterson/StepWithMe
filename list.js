befda5e260955ab1038cf612c14a214f7023d414
j-watterson <jonathonwatterson@gmail.com>
2023-12-19 08:41:27 -0800

    Update list.js

--- list.js
+++ list.js
@@ -4,7 +4,18 @@ var itemList = [
     ["/shoes/altra-escalante-2.5", "https://www.altrarunning.com/shop/men-39%3Bs-escalante-25-al0a4vqa", true],
     ["/shoes/inov-8-bare-xf-210-v3", "https://www.inov-8.com/us/bare-xf-210-v3-mens-natural-training-shoe", true],
     ["/shoes/merrell-trail-glove-7-gore-tex", "https://www.merrell.com/US/en/trail-glove-7-gore-tex/58270M.html", true],
-    // ... add more items as needed
+    ["/shoes/merrell-vapor-glove-3-luna-leather", "https://www.merrell.com/UK/en_GB/vapor-glove-3-luna-leather/33382M.html", true],
+    /*["/shoes/", "linky", true],
+    ["/shoes/", "linky", true],
+    ["/shoes/", "linky", true],
+    ["/shoes/", "linky", true],
+    ["/shoes/", "linky", true],
+    ["/shoes/", "linky", true],
+    ["/shoes/", "linky", true],
+    ["/shoes/", "linky", true],
+    ["/shoes/", "linky", true],
+    ["/shoes/", "linky", true],
+    // ... add more items as needed*/
 ];
 
 // Export the itemList array for use in other files (if using modules)
