0205c4a62435a3c9c03a146f07075cdb7f4963b8
j-watterson <jonathonwatterson@gmail.com>
2023-12-19 07:46:22 -0800

    Update selectAd.js

--- d/selectAd.js
+++ d/selectAd.js
@@ -1,8 +1,9 @@
 // selectAd.js
 function selectAd() {
 
-    var adLink = "https://ads.stepwith.me/shoes/merrell-trail-glove-7-gore-tex.html";
-    var adImage = "https://ads.stepwith.me/shoes/merrell-trail-glove-7-gore-tex.png";
+    var adID = "shoes/merrell-trail-glove-7-gore-tex"
+    var adLink = "https://ads.stepwith.me/" + adID + ".html";
+    var adImage = "https://ads.stepwith.me/" + adID + ".png";
 
     document.getElementById('ad-link').href = adLink;
     document.getElementById('ad-image').src = adImage;
