7d1e7b5dd26f954294106e845ceb5add1adeee86
j-watterson <jonathonwatterson@gmail.com>
2023-12-19 08:06:03 -0800

    Create list.js

---
+++ list.js
@@ -0,0 +1,12 @@
+// Define a 2D array with items
+// Each item has three components: [ID, URL, Boolean]
+var itemList = [
+    ["id1", "http://example.com/page1", true],
+    ["id2", "http://example.com/page2", false],
+    ["id3", "http://example.com/page3", true],
+    // ... add more items as needed
+];
+
+// Export the itemList array for use in other files (if using modules)
+// Uncomment the line below if you're using modules
+// export { itemList };
