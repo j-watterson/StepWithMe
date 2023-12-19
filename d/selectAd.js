641b7462ac91adcd5d48e1c0d5fb5043f82b356a
j-watterson <jonathonwatterson@gmail.com>
2023-12-19 08:16:49 -0800

    Update selectAd.js

--- d/selectAd.js
+++ d/selectAd.js
@@ -1,18 +1,15 @@
 // Function to get the ad ID
+// Assuming itemList is available globally
+// If using modules, import itemList from 'list.js'
+
 function getAdId() {
-    var adIds = [
-        "/shoes/merrell-trail-glove-7-gore-tex",
-        "/apparel/nike-dri-fit-shirt",
-        "/accessories/adidas-running-cap",
-        // ... add more ad IDs here
-    ];
+    // Generate a random index based on the itemList length
+    var randomIndex = Math.floor(Math.random() * itemList.length);
 
-    // Generate a random index based on the array length
-    var randomIndex = Math.floor(Math.random() * adIds.length);
-
-    // Return a random ad ID from the array
-    return adIds[randomIndex];
+    // Return the ID (first element) of a random item from itemList
+    return itemList[randomIndex][0];
 }
+
 
 // Function to select and display an ad
 function selectAd() {
