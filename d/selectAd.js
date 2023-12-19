e681f7543a61555b970eb0bc0b02b34aeb35eede
j-watterson <jonathonwatterson@gmail.com>
2023-12-19 07:52:17 -0800

    Update selectAd.js

--- d/selectAd.js
+++ d/selectAd.js
@@ -3,14 +3,14 @@ function getAdId() {
     // Logic to retrieve ad ID goes here.
     // For example, this could be a random number, or fetched from a server, etc.
     // Here's a placeholder return value for demonstration:
-    return "merrell-trail-glove-7-gore-tex";
+    return "/shoes/merrell-trail-glove-7-gore-tex";
 }
 
 // Function to select and display an ad
 function selectAd() {
     var adId = getAdId(); // Retrieve the ad ID using the new function
-    var adLink = "https://ads.stepwith.me/shoes/" + adId + ".html";
-    var adImage = "https://ads.stepwith.me/shoes/" + adId + ".png";
+    var adLink = "https://ads.stepwith.me" + adId + ".html";
+    var adImage = "https://ads.stepwith.me" + adId + ".png";
 
     document.getElementById('ad-link').href = adLink;
     document.getElementById('ad-image').src = adImage;
