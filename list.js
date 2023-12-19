cd4737dc6a2fdd405366bacb56c832172171179a
j-watterson <jonathonwatterson@gmail.com>
2023-12-19 08:29:24 -0800

    Update list.js

--- list.js
+++ list.js
@@ -1,9 +1,9 @@
 // Define a 2D array with items
 // Each item has three components: [ID, URL, Boolean]
 var itemList = [
-    ["shoes/altra-escalante-2.5", "https://www.altrarunning.com/shop/men-39%3Bs-escalante-25-al0a4vqa", true],
-    ["shoes/inov-8-bare-xf-210-v3", "https://www.inov-8.com/us/bare-xf-210-v3-mens-natural-training-shoe", true],
-    ["shoes/merrell-trail-glove-7-gore-tex", "https://www.merrell.com/US/en/trail-glove-7-gore-tex/58270M.html", true],
+    ["/shoes/altra-escalante-2.5", "https://www.altrarunning.com/shop/men-39%3Bs-escalante-25-al0a4vqa", true],
+    ["/shoes/inov-8-bare-xf-210-v3", "https://www.inov-8.com/us/bare-xf-210-v3-mens-natural-training-shoe", true],
+    ["/shoes/merrell-trail-glove-7-gore-tex", "https://www.merrell.com/US/en/trail-glove-7-gore-tex/58270M.html", true],
     // ... add more items as needed
 ];
 
