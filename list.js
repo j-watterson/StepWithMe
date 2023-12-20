b5be96a756bde63ac8b93c513d3a851d0321df44
j-watterson <jonathonwatterson@gmail.com>
2023-12-20 09:13:34 -0800

    Update list.js

--- list.js
+++ list.js
@@ -3,6 +3,7 @@
 var itemList = [
     ['/shoes/altra-escalante-2.5', 'https://www.altrarunning.com/shop/men-39%3Bs-escalante-25-al0a4vqa', true],
     ['/shoes/inov-8-bare-xf-210-v3', 'https://www.inov-8.com/us/bare-xf-210-v3-mens-natural-training-shoe', true],
+    ['/shoes/joe-nimble-wandertoes-2.0', 'https://uk.joe-nimble.com/products/wandertoes-w-black', true],
     ['/shoes/lems-primal-2', 'https://www.lemsshoes.com/products/mens-primal-2?variant=32230129827898', true],
     ['/shoes/merrell-trail-glove-7-gore-tex', 'https://www.merrell.com/US/en/trail-glove-7-gore-tex/58270M.html', true],
     ['/shoes/merrell-vapor-glove-3-luna-leather', 'https://www.merrell.com/UK/en_GB/vapor-glove-3-luna-leather/33382M.html', true],
