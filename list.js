79768e9ea8ed697a495c627344f37f7c2c19e897
j-watterson <jonathonwatterson@gmail.com>
2023-12-20 13:53:48 -0800

    Update list.js
    /sandals/bedrock-cairn-adventure

--- list.js
+++ list.js
@@ -1,6 +1,7 @@
 // Define a 2D array with items
 // Each item has three components: [ID, URL, Boolean]
 var itemList = [
+    ['/sandals/bedrock-cairn-adventure','https://bedrocksandals.com/products/cairn-sandals',true],
     ['/shoes/altra-escalante-2.5', 'https://www.altrarunning.com/shop/men-39%3Bs-escalante-25-al0a4vqa', true],
     ['/shoes/altra-solstice-xt-2', 'https://www.altrarunning.com/shop/mens-shoes-gym-training/mens-solstice-xt-2-al0a546v', true],
     ['/shoes/inov-8-bare-xf-210-v3', 'https://www.inov-8.com/us/bare-xf-210-v3-mens-natural-training-shoe', true],
@@ -23,7 +24,7 @@ var itemList = [
     ['/shoes/vivobarefoot-primus-trail-ii-fg', 'https://www.vivobarefoot.com/us/primus-trail-ii-fg-mens-ss22', true],
     ['/shoes/vivobarefoot-tracker-fg', 'https://www.vivobarefoot.com/us/tracker-ii-fg-mens-ss22', true],
     ['/shoes/xero-prio-men', 'https://xeroshoes.com/shop/shoes/prio-men/', true],
-    /*['/shoes/','url',true]*/
+    /*['/shoes/','url',true],*/
 ];
 
 // Function to lookup redirect URL by ID
